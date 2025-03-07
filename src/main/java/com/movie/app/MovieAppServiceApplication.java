0031d2bd7703af49df2cde27f66d295dded27999
Pradeep Patil <pradeep.pucsd@gmail.com>
2025-03-07 01:43:12 +0100

    Initial commit for Project setup: Used spring boot framework, java 21 and maven

---
+++ src/main/java/com/movie/app/MovieAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.movie.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MovieAppServiceApplication.class, args);
+  }
+
+}
